8a86c2420dda5824a3cabbc16fb8293ceef37f39
JonesRandom <jones.random97@gmail.com>
2016-09-29 21:40:13 +0800

    Add Header Setting

---
+++ app/src/main/java/com/JonesRandom/JadwalPelajaran/HeaderSetting.java
@@ -0,0 +1,13 @@
+package com.JonesRandom.JadwalPelajaran;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HeaderSetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_header_setting);
+    }
+}
